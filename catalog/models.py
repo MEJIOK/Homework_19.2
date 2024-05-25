6a8c7d736a26abd782e7d5c0352fc6da43d44e17
bak3r <aa.erokhov@outlook.com>
2024-05-25 20:26:24 +0500

    Added manufactured_at

--- catalog/models.py
+++ catalog/models.py
@@ -34,6 +34,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    manufactured_at = models.DateField(null=True, blank=True)  # Новое поле
 
     def __str__(self):
         return self.name

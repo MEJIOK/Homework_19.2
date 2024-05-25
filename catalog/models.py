d4bc8873970c225ae02e6afdf0d53c76f131b2d0
bak3r <aa.erokhov@outlook.com>
2024-05-25 19:09:38 +0500

    Completed 2nd and 3rd tasks from HW 20.1 Added DB 'orders'

--- catalog/models.py
+++ catalog/models.py
@@ -9,3 +9,37 @@ class Contact(models.Model):
 
     def __str__(self):
         return self.name
+
+
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Categories"
+        ordering = ['name']
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='product_images/')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+#     class Meta:
+#         verbose_name = 'студент' # Настройка для наименования одного объекта
+#         verbose_name_plural = 'студенты' # Настройка для наименования набора объектов
--- catalog/urls.py
+++ catalog/urls.py
@@ -7,4 +7,8 @@ app_name = CatalogConfig.name
 urlpatterns = [
     path('', views.home, name='home'),
     path('contacts/', views.contacts, name='contacts'),
+    path('', include('catalog.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

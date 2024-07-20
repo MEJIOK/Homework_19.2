31a72f2eba433109b5889ecea671114d5fe15da4
bak3r <aa.erokhov@outlook.com>
2024-07-20 11:56:50 +0500

    Added product page and images (20.2)

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,16 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
-from catalog.views import home, contacts
+from catalog.views import index, contacts, product_detail
 
 app_name = 'catalog'
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('', index, name='home'),
+    path('product/<int:pk>/', product_detail, name='product_detail'),
     path('contacts/', contacts, name='contacts')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,19 @@
 import datetime
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from catalog.models import Product
 
 
-def home(request):
-    return render(request, 'home.html')
+def index(request):
+    stuff = Product.objects.all()
+    context = {'stuff': stuff}
+    return render(request, 'stuff_list.html', context)
+
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {'product': product}
+    return render(request, 'product_detail.html', context)
 
 
 def contacts(request):

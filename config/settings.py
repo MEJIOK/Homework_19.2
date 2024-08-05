291a7366df0f2085957b93f2ed70f14a96d4b286
bak3r <aa.erokhov@outlook.com>
2024-08-06 01:02:55 +0500

    Add users app

--- config/settings.py
+++ config/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'catalog',
     'blog',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -117,3 +118,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+AUTH_USER_MODEL = 'users.User'
---
+++ users/urls.py
@@ -0,0 +1,8 @@
+from catalog.apps import CatalogConfig
+from users.apps import UsersConfig
+
+app_name = UsersConfig.name
+
+urlpatterns = [
+
+]
